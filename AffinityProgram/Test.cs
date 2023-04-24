6a9676baab73079b709c07fc3f730ec6d8584993
OgzYzc <Zabotrian@outlook.com>
2023-04-24 15:43:03 +0300

    Added test class for future implementation.

--- AffinityProgram/Test.cs
+++ AffinityProgram/Test.cs
@@ -8,5 +8,30 @@ namespace AffinityProgram
 {
     internal class Test
     {
+        //Console.WriteLine("Enter core numbers as integers separated by a space: ");
+        //    string input = Console.ReadLine();
+        //int[] cores = input.Split(' ').Select(int.Parse).ToArray();
+
+        //int coreNumber = 0;
+        //    // Take the user input as power of 2 to make everything easier
+        //    foreach (int core in cores)
+        //    {
+        //        coreNumber += (int) Math.Pow(2, core);
+
+
+        //byte[] hexBytes = BitConverter.GetBytes(coreNumber);
+        //Array.Reverse(hexBytes);
+
+        //    // Pseudo code for this line is like this
+        //    // if (return b if b is equal to 0) is true skip it
+        //    // which converts to trim the first zeros 
+        //    hexBytes = hexBytes.SkipWhile(b => b == 0).Reverse().ToArray();
+
+        ////using replace because if we want to print it add - between values
+        //string hexCoreNumber = BitConverter.ToString(hexBytes).Replace("-", "");
+
+        //Console.WriteLine("0x" + hexCoreNumber);
+
+        //    Console.ReadKey();
     }
 }
