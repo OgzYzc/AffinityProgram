5f67a49e477c1da399190e24a48433b591c4c452
OgzYzc <OgzYzc@outlook.com>
2023-09-20 03:13:45 +0300

    Reworked on NIC settings for readablility.

--- AffinityProgram/Controller/Controller_Set/Controller_SetNicEntries/Controller_SetNicPowershell.cs
+++ AffinityProgram/Controller/Controller_Set/Controller_SetNicEntries/Controller_SetNicPowershell.cs
@@ -1,6 +1,7 @@
 ﻿using AffinityProgram.Find_Core;
 using AffinityProgram.Model;
 using System;
+using System.Linq;
 using System.Management.Automation;
 using System.Threading;
 
@@ -10,187 +11,128 @@ namespace AffinityProgram.Controller.Controller_SetNicPowershell
     {
         public static void Run()
         {
-            
             bool IsSmtEnabled = View.MainMenu.isSmtEnabled;
-
             try
             {
-                // Find currently adapter name user using
                 using (PowerShell powershell = PowerShell.Create())
                 {
-                    // Get all physical network adapters' name
-                    powershell.AddCommand("Get-NetAdapter").AddParameter("Name", "*").AddParameter("Physical");
-                    var adapters = powershell.Invoke();
-
+                    var adapters = getAllPhysicalAdapters(powershell);
                     foreach (var adapter in adapters)
                     {
-                        // Get the adapter name from the current adapter object
-                        string adapterName = adapter.Properties["Name"].Value.ToString();
-                        Console.WriteLine($"Adapter name : '{adapterName}' ");
-                        
-                        try
-                        {
-                            powershell.Commands.Clear();
-                            // Numberofreceivequeue workaround
-                            Console.WriteLine($"Selecting max processors for '1' CPUs '{adapterName}'.");
-                            powershell.AddCommand("Set-NetAdapterRss")
-                                             .AddParameter("Name", adapterName)
-                                             .AddParameter("MaxProcessors", 1)
-                                             .Invoke();
-
-                            if (Find_Core_CPPC.selectedCoreNIC == null)
-                            {
-                                Console.WriteLine("You are adding affinity without using CPPC. " +
-                                    "If you enabled CPPC go back to menu and press 'Find best core' then come back." +
-                                    "Or you can add predetermined affinity. Press Enter for adding Predetermined affinity.");
-
-                                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-                                if (keyInfo.Key == ConsoleKey.Enter)
-                                {
-                                    if (IsSmtEnabled)
-                                    {
-                                        // Bind the base processor to CPU for RSS
-                                        Console.WriteLine($"Binding the base processor to CPU '4' for RSS on '{adapterName}'.");
-                                        powershell.AddCommand("Set-NetAdapterRss")
-                                                         .AddParameter("Name", adapterName)
-                                                         .AddParameter("BaseProcessorNumber", 4)
-                                                         .Invoke();
-
-                                        powershell.AddCommand("Set-NetAdapterRss")
-                                             .AddParameter("Name", adapterName)
-                                             .AddParameter("MaxProcessornumber", 5)
-                                             .Invoke();
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine($"Binding the base processor to CPU '2' for RSS on '{adapterName}'.");
-                                        powershell.AddCommand("Set-NetAdapterRss")
-                                                         .AddParameter("Name", adapterName)
-                                                         .AddParameter("BaseProcessorNumber", 2)
-                                                         .Invoke();
-
-                                        powershell.AddCommand("Set-NetAdapterRss")
-                                             .AddParameter("Name", adapterName)
-                                             .AddParameter("MaxProcessornumber", 3)
-                                             .Invoke();
-                                    }
-                                }
-                                else
-                                    break;
-                            }
-                            else
-                            {
-                                var selectedCore = Math.Log(Find_Core_CPPC.selectedCoreNIC[0], 2);
-
-                                // Bind the base processor to selected CPU for RSS
-                                Console.WriteLine($"Binding the base processor to CPU '{selectedCore}' for RSS on '{adapterName}'.");
-                                powershell.AddCommand("Set-NetAdapterRss")
-                                                 .AddParameter("Name", adapterName)
-                                                 .AddParameter("BaseProcessorNumber", selectedCore)
-                                                 .Invoke();
-
-                                powershell.AddCommand("Set-NetAdapterRss")
-                                             .AddParameter("Name", adapterName)
-                                             .AddParameter("MaxProcessornumber", selectedCore + 1)
-                                             .Invoke();
-                            }
-
-                            //// Verify the base processor for RSS
-                            //Console.WriteLine($"Verifying the base processor for RSS on '{adapterName}'.");
-                            //powershell.Commands.Clear();
-                            //powershell.AddCommand("Get-NetAdapterRss")
-                            //                 .AddParameter("Name", adapterName);
-                            //var results = powershell.Invoke();
-                            //if (results.Count > 0)
-                            //{
-                            //    var rss = results[0];
-                            //    var baseProcessorNumber = rss.Properties["BaseProcessorNumber"].Value.ToString();
-                            //    if (Find_Core_CPPC.selectedCoreNIC == null)
-                            //    {
-                            //        if (IsSmtEnabled)
-                            //        {
-                            //            if (baseProcessorNumber == "4")
-                            //            {
-                            //                Console.WriteLine($"Successfully bound the base processor to CPU '4' for RSS on '{adapterName}'.");
-                            //            }
-                            //            else
-                            //            {
-                            //                Console.WriteLine($"Failed to bind the base processor to CPU '4' for RSS on '{adapterName}'.");
-                            //            }
-                            //        }
-                            //        else
-                            //        {
-                            //            if (baseProcessorNumber == "2")
-                            //            {
-                            //                Console.WriteLine($"Successfully bound the base processor to CPU '2' for RSS on '{adapterName}'.");
-                            //            }
-                            //            else
-                            //            {
-                            //                Console.WriteLine($"Failed to bind the base processor to CPU '2' for RSS on '{adapterName}'.");
-                            //            }
-                            //        }                                        
-                            //    }
-                            //    else
-                            //    {
-                            //        if (baseProcessorNumber == selectedCore.ToString())
-                            //        {
-                            //            Console.WriteLine($"Successfully bound the base processor to CPU '{selectedCore}' for RSS on '{adapterName}'.");
-                            //        }
-                            //        else
-                            //        {
-                            //            Console.WriteLine($"Failed to bind the base processor to CPU '{selectedCore}' for RSS on '{adapterName}'.");
-                            //        }
-                            //    }
-
-                            //}
-                            
-                            // Restart the current adapter
-                            Console.WriteLine($"Restarting adapter '{adapterName}'\nWaiting 10 seconds for the adapter to restart itself.");
-                            powershell.Commands.Clear();
-                            powershell.AddCommand("Restart-NetAdapter")
-                                             .AddParameter("Name", adapterName)
-                                             .Invoke();
-
-                            powershell.Commands.Clear();
-                            // Settings max processors to 2. This set Number of receive queue to 2
-                            Console.WriteLine($"Selecting max processors for '2' CPUs '{adapterName}'.");
-                            powershell.AddCommand("Set-NetAdapterRss")
-                                             .AddParameter("Name", adapterName)
-                                             .AddParameter("MaxProcessors", 2)
-                                             .Invoke();
-
-                            // Restart the current adapter
-                            Console.WriteLine($"Restarting adapter '{adapterName}'\nWaiting 10 seconds for the adapter to restart itself.");
-                            powershell.Commands.Clear();
-                            powershell.AddCommand("Restart-NetAdapter")
-                                             .AddParameter("Name", adapterName)
-                                             .Invoke();
-                            Thread.Sleep(10000);
-
-                            // Check the adapter running or not
-                            powershell.Commands.Clear();
-                            powershell.AddScript($"Get-NetAdapter | Where-Object {{ $_.Name -eq '{adapterName}' -and $_.Status -eq 'Up' }}");
-                            var checkResults = powershell.Invoke();
-                            if (checkResults.Count > 0)
-                            {
-                                Console.WriteLine($"Adapter '{adapterName}' restarted successfully.\nYou can go back now.");
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Failed to restart adapter '{adapterName}'.\nGo back and try again.");
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"An error occurred while processing adapter '{adapterName}'\nGo back and try again.: {ex.Message}");
-                        }                        
+                        processAdapter(powershell, adapter, IsSmtEnabled);
                     }
-                    powershell.Dispose();
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static PSObject[] getAllPhysicalAdapters(PowerShell powershell)
+        {
+            powershell.AddCommand("Get-NetAdapter").AddParameter("Name", "*").AddParameter("Physical");
+            return powershell.Invoke().ToArray();
+        }
+
+        private static void processAdapter(PowerShell powershell, PSObject adapter, bool IsSmtEnabled)
+        {
+            string adapterName = adapter.Properties["Name"].Value.ToString();
+            Console.WriteLine($"Processing Adapter: '{adapterName}'");
+
+            try
+            {
+                //NORQ workaround
+                setMaxProcessors(powershell, adapterName, 1);
+
+                if (Find_Core_CPPC.selectedCoreNIC == null)
+                {
+                    predeterminedAffinity(powershell, adapterName, IsSmtEnabled);
+                }
+                else
+                {
+                    selectedAffinity(powershell, adapterName);
+                }
+
+                restartAdapter(powershell, adapterName);
+                setMaxProcessors(powershell, adapterName, 4);
+                restartAdapter(powershell, adapterName);
+
+                verifyAdapterStatus(powershell, adapterName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error with adapter '{adapterName}': {ex.Message}");
+            }
+        }
+
+        private static void setMaxProcessors(PowerShell powershell, string adapterName, int maxProcessors)
+        {
+            Console.WriteLine($"Selecting max processors for '{maxProcessors}' CPUs '{adapterName}'.");
+            powershell.Commands.Clear();
+            powershell.AddCommand("Set-NetAdapterRss")
+                      .AddParameter("Name", adapterName)
+                      .AddParameter("MaxProcessors", maxProcessors)
+                      .Invoke();
+        }
+
+        private static void predeterminedAffinity(PowerShell powershell, string adapterName, bool IsSmtEnabled)
+        {
+            Console.WriteLine("You are adding affinity without using CPPC. " +
+                "If you enabled CPPC go back to menu and press 'Find best core' then come back." +
+                "Or you can add predetermined affinity. Press Enter for adding Predetermined affinity.");
+
+            if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+            {
+                Console.Clear();
+                int baseProcessor = IsSmtEnabled ? 4 : 0;
+                int maxProcessor = IsSmtEnabled ? 6 : 4;
+
+                bindBaseProcessor(powershell, adapterName, baseProcessor);
+                setMaxProcessors(powershell, adapterName, maxProcessor);
+            }
+        }
+
+        private static void selectedAffinity(PowerShell powershell, string adapterName)
+        {
+            var selectedCore = (int)Math.Log(Find_Core_CPPC.selectedCoreNIC[0], 2);
+            bindBaseProcessor(powershell, adapterName, selectedCore);
+            setMaxProcessors(powershell, adapterName, selectedCore + 1);
+        }
+
+        private static void bindBaseProcessor(PowerShell powershell, string adapterName, int baseProcessor)
+        {
+            Console.WriteLine($"Binding the base processor to CPU '{baseProcessor}' for RSS on '{adapterName}'.");
+            powershell.Commands.Clear();
+            powershell.AddCommand("Set-NetAdapterRss")
+                      .AddParameter("Name", adapterName)
+                      .AddParameter("BaseProcessorNumber", baseProcessor)
+                      .Invoke();
+        }
+
+        private static void restartAdapter(PowerShell powershell, string adapterName)
+        {
+            Console.WriteLine($"Restarting adapter '{adapterName}'\nWaiting 10 seconds for the adapter to restart.");
+            powershell.Commands.Clear();
+            powershell.AddCommand("Restart-NetAdapter")
+                      .AddParameter("Name", adapterName)
+                      .Invoke();
+            Thread.Sleep(10000);
+        }
+
+        private static void verifyAdapterStatus(PowerShell powershell, string adapterName)
+        {
+            powershell.Commands.Clear();
+            powershell.AddScript($"Get-NetAdapter | Where-Object {{ $_.Name -eq '{adapterName}' -and $_.Status -eq 'Up' }}");
+            var checkResults = powershell.Invoke();
+
+            if (checkResults.Count > 0)
+            {
+                Console.WriteLine($"Adapter '{adapterName}' restarted successfully.\nYou can go back now.");
+            }
+            else
+            {
+                Console.WriteLine($"Failed to restart adapter '{adapterName}'.\nGo back and try again.");
             }
         }
     }
--- AffinityProgram/Controller/Controller_Set/Controller_SetNicEntries/Controller_SetNicRegistry.cs
+++ AffinityProgram/Controller/Controller_Set/Controller_SetNicEntries/Controller_SetNicRegistry.cs
@@ -3,6 +3,7 @@ using AffinityProgram.Find_Core;
 using AffinityProgram.Model;
 using Microsoft.Win32;
 using System;
+using System.Collections.Generic;
 using System.Management.Automation;
 using System.Security.AccessControl;
 using System.Security.Principal;
@@ -12,70 +13,153 @@ namespace AffinityProgram.Controller.Controller_SetNicRegistry
 {
     internal class Controller_SetNicRegistry
     {
+        private const string ndisRegistryPath = @"SYSTEM\CurrentControlSet\Services\NDIS\Parameters";
+        private const string tcpipRegistryPath = @"SYSTEM\CurrentControlSet\Services\Tcpip";
+        private const string adapterRegistryPath = @"SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0001"; //Need to work for this
+
         public Controller_SetNicRegistry()
         {
-            bool IsSmtEnabled = View.MainMenu.isSmtEnabled;
-
             try
             {
-                //Adding RssBaseCpu to Ndis service
-                var ndisRegistryPath = new Model_RegistryPath(@"SYSTEM\CurrentControlSet\Services\NDIS\Parameters");
-                var tcpipRegistryPath = new Model_RegistryPath(@"SYSTEM\CurrentControlSet\Services\Tcpip");
+                setNdisKeyValues();
+                setTcpipKeyValues();
+                setDriverKeyValues();
 
-                var regSecurity = new RegistrySecurity();
-                regSecurity.AddAccessRule(new RegistryAccessRule(new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, null), RegistryRights.FullControl, InheritanceFlags.None, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
-
-                using (var ndisKey = Registry.LocalMachine.CreateSubKey(ndisRegistryPath.RegistryPath, RegistryKeyPermissionCheck.ReadWriteSubTree, regSecurity))
-                {
-
-                    if (Find_Core_CPPC.selectedCoreNIC == null)
-                    {
-                        Console.WriteLine("You are adding affinity without using CPPC. " +
-                            "If you enabled CPPC go back to menu and press 'Find best core' then come back." +
-                            "Or you can add predetermined affinity. Press Enter for adding Predetermined affinity.");
-
-                        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-                        if (keyInfo.Key == ConsoleKey.Enter)
-                        {
-                            if (View.MainMenu.isSmtEnabled)
-                            {
-                                ndisKey.SetValue("RssBaseCpu", "4", RegistryValueKind.DWord);
-                            }
-                            else
-                            {
-                                ndisKey.SetValue("RssBaseCpu", "2", RegistryValueKind.DWord);
-                            }
-                        }
-                        else
-                            return;
-                    }
-                    else
-                    {
-                        var selectedCore = Math.Log(Find_Core_CPPC.selectedCoreNIC[0], 2);
-                        ndisKey.SetValue("RssBaseCpu", selectedCore, RegistryValueKind.DWord);
-                    }
-
-                    //Use max 2 core on either choice
-                    ndisKey.SetValue("MaxNumRssCpus", "2", RegistryValueKind.DWord);
-
-                    using (var tcpipKey = Registry.LocalMachine.CreateSubKey(tcpipRegistryPath.RegistryPath, RegistryKeyPermissionCheck.ReadWriteSubTree, regSecurity))
-                    {
-                        tcpipKey.SetValue("DisableTaskOffload","0",RegistryValueKind.DWord);
-                    }
-
-                    Console.WriteLine("Registry key added successfully.\nExecuting powershell.");
-                }
-
-
-                //Waiting 3 seconds for dramatic effect
+                // Waiting for a short time before executing the next action
                 Thread.Sleep(3000);
-
+                Console.Clear();
                 Controller_SetNicPowershell.Controller_SetNicPowershell.Run();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        private RegistrySecurity GetRegistrySecurity()
+        {
+            var regSecurity = new RegistrySecurity();
+            regSecurity.AddAccessRule(new RegistryAccessRule(new SecurityIdentifier(WellKnownSidType.BuiltinUsersSid, null),
+                RegistryRights.FullControl, InheritanceFlags.None, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
+            return regSecurity;
+        }
+
+        private void setNdisKeyValues()
+        {
+            using (var ndisKey = Registry.LocalMachine.CreateSubKey(ndisRegistryPath, RegistryKeyPermissionCheck.ReadWriteSubTree, GetRegistrySecurity()))
+            {
+                if (Find_Core_CPPC.selectedCoreNIC == null)
+                {
+                    Console.WriteLine("You are adding affinity without using CPPC. " +
+                        "If you enabled CPPC go back to menu and press 'Find best core' then come back." +
+                        "Or you can add predetermined affinity. Press Enter for adding Predetermined affinity.");
+
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    if (keyInfo.Key == ConsoleKey.Enter)
+                    {
+                        if (View.MainMenu.isSmtEnabled)
+                        {
+                            ndisKey.SetValue("RssBaseCpu", "4", RegistryValueKind.DWord);
+                        }
+                        else
+                        {
+                            ndisKey.SetValue("RssBaseCpu", "0", RegistryValueKind.DWord);
+                        }
+                    }
+                    else
+                        return;
+                }
+                else
+                {
+                    var selectedCore = Math.Log(Find_Core_CPPC.selectedCoreNIC[0], 2);
+                    ndisKey.SetValue("RssBaseCpu", selectedCore, RegistryValueKind.DWord);
+                }
+
+                // Use max 2 core on either choice
+                ndisKey.SetValue("MaxNumRssCpus", "4", RegistryValueKind.DWord);
+            }
+
+            Console.WriteLine("Registry key added successfully.\nExecuting powershell.");
+        }
+
+        private void setTcpipKeyValues()
+        {
+            using (var tcpipKey = Registry.LocalMachine.CreateSubKey(tcpipRegistryPath, RegistryKeyPermissionCheck.ReadWriteSubTree, GetRegistrySecurity()))
+            {
+                tcpipKey.SetValue("DisableTaskOffload", "0", RegistryValueKind.DWord);
+            }
+        }
+
+        private void setDriverKeyValues()
+        {
+            var nicValues = new Dictionary<string, string>()
+        {
+                //RSS                
+                { "*RSS", "1" },
+                { "*RSSProfile", "4" },
+                { "*RssBaseProcNumber", "0" },
+                { "*MaxRssProcessors", "4" },
+                { "*NumaNodeId", "0" },
+                { "*NumRssQueues", "4" },
+                { "*RssBaseProcGroup", "0" },
+                { "*RssMaxProcNumber", "3" },
+                { "*RssMaxProcGroup", "0" },
+
+                //Latency
+                { "*InterruptModeration", "0" },
+                { "ITR", "0" },
+                { "*LsoV1IPv4", "0" },
+                { "*LsoV2IPv4", "0" },
+                { "*LsoV1IPv6", "0" },
+                { "*LsoV2IPv6", "0" },
+                { "*FlowControl", "0" },
+                { "TxIntDelay", "8" },
+                { "EnableDCA", "1" },
+                { "DMACoalescing", "0" },
+                { "*TransmitBuffers", "1600" },
+                { "*ReceiveBuffers", "1600" },
+                { "EnableLLI", "1" },
+                { "EnableModernStandby", "0" },
+                { "EnablePME", "0" },
+                { "*PMNSOffload", "0" },
+                { "*PMARPOffload", "0" },                
+
+                //These ones are listed in intel linux driver documentation. Not sure it works.
+                //downloadmirror.intel.com/15817/eng/readme.txt
+                { "RxIntDelay", "0" },
+                { "RxAbsIntDelay", "0" },
+                { "TxAbsIntDelay", "0" },
+
+        };
+
+            using (var driverKey = Registry.LocalMachine.CreateSubKey(adapterRegistryPath, RegistryKeyPermissionCheck.ReadWriteSubTree, GetRegistrySecurity()))
+            {
+                foreach (var entry in nicValues)
+                {
+                    driverKey.SetValue(entry.Key, entry.Value, RegistryValueKind.String);
+
+                    //Add everyport for low latency interrupt.
+                    string[] lliPorts = new string[65535];
+                    for (int i = 0; i < 65535; i++)
+                    {
+                        lliPorts[i] = (i + 1).ToString();
+                    }
+
+                    driverKey.SetValue("LLIPorts", lliPorts, RegistryValueKind.MultiString);
+                }
+
+                
+                
+            }
+
+            
+
+            // Set the value in the registry
+            
+
+            Console.WriteLine("LLIPorts added to the registry.");
+
+            Console.WriteLine("Driver values are added.");
         }
     }
 }
