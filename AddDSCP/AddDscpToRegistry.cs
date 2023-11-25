e9bb925d3d2c3cb50cf9fac9564d54e77c8774b0
OgzYzc <ogzyzc@outlook.com>
2023-11-25 14:46:41 +0300

    Fixed a bug that dscp values adding registry constantly.

--- AddDSCP/AddDscpToRegistry.cs
+++ AddDSCP/AddDscpToRegistry.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Security.AccessControl;
 using System.Security.Principal;
 
@@ -14,6 +15,46 @@ namespace AddDSCP
         {
             private static Dictionary<string, int> gameNameCounter = new Dictionary<string, int>();
 
+            public static void AddRegistry()
+            {
+                try
+                {
+                    //Clearing dictionary for not add additional key with unique number
+                    gameNameCounter.Clear();
+                    string path = @"SOFTWARE\Policies\Microsoft\Windows\QoS";
+
+                    using (RegistryKey qosKey = Registry.LocalMachine.CreateSubKey(path, RegistryKeyPermissionCheck.ReadWriteSubTree, getRegistrySecurity()))
+                    {
+                        foreach (var item in ReadJsonLibraryFolder.Read.gamePaths)
+                        {
+                            string gameName = GetGameNameFromPath(item);
+
+                            Console.WriteLine("Adding registry key for " + gameName);
+                            using (RegistryKey gameKey = qosKey.CreateSubKey(gameName))
+                            {
+                                gameKey.SetValue("Application Name", item, RegistryValueKind.String);
+                                gameKey.SetValue("DSCP Value", "46", RegistryValueKind.String);
+                                gameKey.SetValue("Local IP", "*", RegistryValueKind.String);
+                                gameKey.SetValue("Local IP Prefix Length", "*", RegistryValueKind.String);
+                                gameKey.SetValue("Local Port", "*", RegistryValueKind.String);
+                                gameKey.SetValue("Protocol", "*", RegistryValueKind.String);
+                                gameKey.SetValue("Remote IP", "*", RegistryValueKind.String);
+                                gameKey.SetValue("Remote IP Prefix Length", "*", RegistryValueKind.String);
+                                gameKey.SetValue("Remote Port", "*", RegistryValueKind.String);
+                                gameKey.SetValue("Throttle Rate", "-1", RegistryValueKind.String);
+                                gameKey.SetValue("Version", "1.0", RegistryValueKind.String);
+
+                            }
+                        }
+
+                        UpdateGroupPolicy();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error : {ex.Message}");
+                }
+            }
             static string GetGameNameFromPath(string path)
             {
                 int commonIndex = path.IndexOf("common", StringComparison.OrdinalIgnoreCase);
@@ -42,44 +83,6 @@ namespace AddDSCP
                 }
                 return path;
             }
-
-            public static void AddRegistry()
-            {
-                try
-                {
-                    string path = @"SOFTWARE\Policies\Microsoft\Windows\QoS";
-
-                    using (RegistryKey qosKey = Registry.LocalMachine.CreateSubKey(path, RegistryKeyPermissionCheck.ReadWriteSubTree, getRegistrySecurity()))
-                    {
-                        foreach (var item in ReadJsonLibraryFolder.Read.gamePaths)
-                        {
-                            string gameName = GetGameNameFromPath(item);
-
-                            using (RegistryKey gameKey = qosKey.CreateSubKey(gameName))
-                            {
-                                gameKey.SetValue("Application Name", item, RegistryValueKind.String);
-                                gameKey.SetValue("DSCP Value", "46", RegistryValueKind.String);
-                                gameKey.SetValue("Local IP", "*", RegistryValueKind.String);
-                                gameKey.SetValue("Local IP Prefix Length", "*", RegistryValueKind.String);
-                                gameKey.SetValue("Local Port", "*", RegistryValueKind.String);
-                                gameKey.SetValue("Protocol", "*", RegistryValueKind.String);
-                                gameKey.SetValue("Remote IP", "*", RegistryValueKind.String);
-                                gameKey.SetValue("Remote IP Prefix Length", "*", RegistryValueKind.String);
-                                gameKey.SetValue("Remote Port", "*", RegistryValueKind.String);
-                                gameKey.SetValue("Throttle Rate", "-1", RegistryValueKind.String);
-                                gameKey.SetValue("Version", "1.0", RegistryValueKind.String);
-                            }
-                        }
-
-                        Console.WriteLine("DSCP values added to registry.");
-                        UpdateGroupPolicy();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error : {ex.Message}");
-                }
-            }
             private static RegistrySecurity getRegistrySecurity()
             {
                 RegistrySecurity regSecurity = new RegistrySecurity();
@@ -96,7 +99,7 @@ namespace AddDSCP
                 p.StartInfo.Arguments = "/C gpupdate /force";
                 p.Start();
                 p.WaitForExit(1000);
-                
+
             }
         }
     }
