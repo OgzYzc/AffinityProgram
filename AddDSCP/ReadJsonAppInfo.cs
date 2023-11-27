b8d08a5e5298e4acbba75ca5970fe7465e1ce7b3
OgzYzc <ogzyzc@outlook.com>
2023-11-27 17:33:47 +0300

    Finding executable values has been enhanced.

--- AddDSCP/ReadJsonAppInfo.cs
+++ AddDSCP/ReadJsonAppInfo.cs
@@ -11,7 +11,6 @@ namespace AddDSCP
         public class Read
         {
             private static string installdirValue;
-            private static bool isInLaunch = false;
             private static HashSet<string> uniqueCombinations = new HashSet<string>();
             static public HashSet<string[]> gameInstalldir;
             public static void ReadAppInfo(string fileName)
@@ -23,7 +22,6 @@ namespace AddDSCP
                 {
                     JsonTokenType tokenType = reader.TokenType;
 
-
                     switch (tokenType)
                     {
                         case JsonTokenType.PropertyName:
@@ -34,53 +32,18 @@ namespace AddDSCP
                                 reader.Read();
                                 installdirValue = reader.GetString();
                             }
-                            else if (propertyName == "launch")
-                            {
-                                isInLaunch = true;
-                            }
-                            else if (isInLaunch && propertyName == "executable")
+                            else if (propertyName == "executable")
                             {
                                 reader.Read();
                                 string executableValue = reader.GetString();
-
-                                while (reader.Read())
+                                if (executableValue.EndsWith(".exe"))
                                 {
-                                    if (reader.TokenType == JsonTokenType.PropertyName && reader.GetString() == "config")
+                                    string combination = $"{installdirValue}: {executableValue}";
+                                    if (uniqueCombinations.Add(combination) && !ContainsExcludedString(combination))
                                     {
-                                        reader.Read();
-                                        while (reader.Read())
-                                        {
-                                            if (reader.TokenType == JsonTokenType.PropertyName && reader.GetString() == "oslist")
-                                            {
-                                                reader.Read();
-
-                                                if (reader.TokenType == JsonTokenType.String && reader.GetString() == "windows")
-                                                {
-                                                    string combination = $"{installdirValue}: {executableValue}";
-                                                    if (uniqueCombinations.Add(combination) && !ContainsExcludedString(combination))
-                                                    {
-                                                        gameInstalldir.Add(new string[] { installdirValue, executableValue });
-                                                    }
-                                                }
-                                            }
-                                            else if (reader.TokenType == JsonTokenType.EndObject)
-                                            {
-                                                break;
-                                            }
-                                        }
-                                    }
-                                    else if (reader.TokenType == JsonTokenType.EndObject)
-                                    {
-                                        break;
+                                        gameInstalldir.Add(new string[] { installdirValue, executableValue });
                                     }
                                 }
-                            }
-                            break;
-
-                        case JsonTokenType.EndObject:
-                            if (isInLaunch)
-                            {
-                                isInLaunch = false;
                             }
                             break;
                     }
@@ -91,10 +54,9 @@ namespace AddDSCP
                     string[] excludedStrings = {
                         "Demo", "SDK", "Server",
                         "test", "link2ea", "Beta",
-                        "sdklauncher","ds","SteamWorks",
+                        "sdklauncher","SteamWorks",
                         "bat","DO NOT USE","vbs",
-                        "app","TwistedDraw","htm"};
-
+                        "app","TwistedDraw","htm","Artwork"};
                     return excludedStrings.Any(s => value.IndexOf(s, StringComparison.OrdinalIgnoreCase) != -1);
                 }
                 File.Delete(Path.Combine(Path.GetTempPath(), "appinfo.json"));
